38c3d8886f09056da2eb352036e96dbcd4f5d31b
Aldiyar Amirov <amirovaldiar@gmail.com>
2023-05-30 22:10:26 +0300

    add new handle

--- src/main/java/com/innopolis/innometrics/authserver/controller/AdminAPI.java
+++ src/main/java/com/innopolis/innometrics/authserver/controller/AdminAPI.java
@@ -10,6 +10,7 @@ import com.innopolis.innometrics.authserver.exceptions.ValidationException;
 import com.innopolis.innometrics.authserver.service.*;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
